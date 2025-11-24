d1b0c131dc28b0898128856986e6948926fd680e
sde-kushal <dev@kushalkumarsaha.com>
2025-11-25 00:57:30 +0530

    148.sort list

---
+++ medium/0148-sort-list/sol.cpp
@@ -0,0 +1,33 @@
+#include <queue>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head) return NULL;
+
+        priority_queue<int,vector<int>,greater<int>> pq;
+        ListNode* ptr = head;
+
+        while(ptr) {
+            pq.push(ptr->val);
+            ptr = ptr->next;
+        }
+
+        ptr = head;
+        while(ptr) {
+            ptr->val = pq.top();
+            pq.pop();
+            ptr = ptr->next;
+        }
+
+        return head;
+    }
+};
